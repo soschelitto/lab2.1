753abfe95b1f88dd4fb084d1b4caf033bcd251ad
vyacheslav <89089477015c@mail.ru>
2019-09-25 11:49:20 +0300

    11-55

---
+++ lab2/skript4.js
@@ -0,0 +1,10 @@
+let X = parseInt(prompt("Введите число"));
+let Y = parseInt(prompt("Введите второе число"));
+let R8 = confirm("Выполнить сложение");
+if ((R8 === true)) {
+alert (X + Y);
+}
+else {
+alert ("error");
+
+}
